1450c21da566be3a2d315249bc2bc9cbf457e480
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-29 23:59:41 +0300

    add fetch test

--- src/index.js
+++ src/index.js
@@ -2,3 +2,40 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  search: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  infoContainer: document.querySelector('.country-info'),
+};
+
+// search.addEventListener('input', fetchCountries);
+
+// function fetchCountries(name) {
+//   return fetch(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(country => {
+//       console.log(country);
+//     });
+// }
+
+// function renderCountryCard(country) {
+// const markup
+// }
+// ===============================
+return fetch(
+  `https://restcountries.com/v3.1/name/deutschland?fields=name,capital,population,flags,languages`
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+    const markup = countryCard(country);
+    refs.infoContainer.innerHTML = markup;
+  })
+  .catch(error => {
+    console.log(error);
+  });
