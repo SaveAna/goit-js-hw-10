7990f61d833fd95b162a250960c50b9ce1a1c964
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-31 03:55:17 +0300

    add listener for input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// import Handlebars from 'handlebars/runtime';
-// import listTpl from './templates/country-list.hbs';
-//import { compile } from 'handlebars';
-// import cardTpl from './templates/country-card.hbs';
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries.js';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -13,23 +11,4 @@ const refs = {
   infoContainer: document.querySelector('.country-info'),
 };
 
-// fetch('./templates/country-card.hbs')
-//   .then(response => response.text())
-//   .then(countryCardTpl => {
-//     const cardTpl = Handlebars.compile(countryCardTpl);
-
-//     return fetch(
-//       `https://restcountries.com/v3.1/name/deutschland?fields=name,capital,population,flags,languages`
-//     )
-//       .then(response => response.json())
-//       .then(country => {
-//         const markup = cardTpl(country);
-//         refs.infoContainer.innerHTML = markup;
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+refs.search.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
