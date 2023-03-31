a6225f55498166649c059d3aad7d58fdd768b848
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-31 04:13:14 +0300

    add render functions for markup

--- src/index.js
+++ src/index.js
@@ -12,3 +12,30 @@ const refs = {
 };
 
 refs.search.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function renderCountryList(countries) {
+  return countries
+    .map(({ flags, name }) => {
+      return `<li class="country-list__item">
+              <img src="${flags.svg}" alt="Flag of ${name.official}" width = 40px height = 30px>
+              <p class="country-list__name">${name.official}</p>
+              </li>`;
+    })
+    .join('');
+}
+
+function renderCountryInfo(countries) {
+  return countries
+    .map(({ flags, name, capital, population, languages }) => {
+      return `<img src='${flags.png}' alt='${flags.alt}'/>
+              <h1>${name.common}</h1>
+              <ul class="country-info__list">
+                <li class="country-info__item"><p>Capital:</p> ${capital}</li>
+                <li class="country-info__item"><p>Population:</p> ${population}</li>
+                <li class="country-info__item"><p>Languages:</p> ${Object.values(
+                  languages
+                )}</li>
+              </ul>`;
+    })
+    .join();
+}
