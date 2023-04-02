eacc87f96992600adabc9a9e16cf59860c1dce34
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-02 04:13:17 +0300

    add onInput function + some markup styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetchCountries.js';
-import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -13,11 +13,40 @@ const refs = {
 
 refs.search.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
+function onInput(event) {
+  let name = event.target.value.trim();
+  cleanSearch();
+  console.log(name);
+  if (!name) {
+    return;
+  }
+
+  fetchCountries(name)
+    .then(response => {
+      if (response.length > 10) {
+        // cleanSearch();
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (response.length === 1) {
+        // refs.list.innerHTML = '';
+        return (refs.infoContainer.innerHTML = renderCountryInfo(response));
+      } else if (response.length >= 2 && response.length <= 10) {
+        refs.list.innerHTML = renderCountryList(response);
+      }
+      // refs.infoContainer.innerHTML = '';
+    })
+
+    .catch(error => {
+      cleanSearch();
+    });
+}
+
 function renderCountryList(countries) {
   return countries
     .map(({ flags, name }) => {
       return `<li class="country-list__item">
-              <img src="${flags.svg}" alt="Flag of ${name.official}" width = 40px height = 30px>
+              <img src="${flags.svg}" alt="Flag of ${name.official}" width = 40px>
               <p class="country-list__name">${name.official}</p>
               </li>`;
     })
@@ -30,12 +59,17 @@ function renderCountryInfo(countries) {
       return `<img src='${flags.svg}' alt='${flags.alt}'/>
               <h1>${name.common}</h1>
               <ul class="country-info__list">
-                <li class="country-info__item"><p>Capital:</p> ${capital}</li>
-                <li class="country-info__item"><p>Population:</p> ${population}</li>
-                <li class="country-info__item"><p>Languages:</p> ${Object.values(
+                <li class="country-info__item"><b>Capital:</b> ${capital}</li>
+                <li class="country-info__item"><b>Population:</b> ${population}</li>
+                <li class="country-info__item"><b>Languages:</b> ${Object.values(
                   languages
                 )}</li>
               </ul>`;
     })
-    .join();
+    .join('');
+}
+
+function cleanSearch() {
+  refs.list.innerHTML = '';
+  refs.infoContainer.innerHTML = '';
 }
